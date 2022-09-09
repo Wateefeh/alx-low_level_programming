c4596ec56fbcad1cc9f92562f4e82e171d3c674e
Boluwatife Arotiba <boluwatifearotiba09@gmail.com>
2022-09-08 17:31:17 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-
 /**
 *main - Entry point
 *Return: Always 1
@@ -8,6 +6,6 @@
 
 int main(void)
 {
-	write(2, "and that pkiece of art is useful\" - Dora Korpar, 2015-10-19\". 59);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\". 59);
 	return (1);
 }

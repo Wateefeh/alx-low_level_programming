70b1bf1881b986adb7f03628fb3d6287411b1bda
Boluwatife Arotiba <boluwatifearotiba09@gmail.com>
2022-09-19 09:59:51 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#indef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char);
+void reset_to_98(int *);
+void swap_int(int *, int *);
+int _strlen(char *);
+void _puts(char *);
+void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *,int);
+void *_strcpy(char *, char *);
+int _atoi(char *);
+
+#endif

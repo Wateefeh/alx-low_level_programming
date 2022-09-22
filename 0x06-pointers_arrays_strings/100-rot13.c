d09db7b01762920e01e2dbefeb3f5e34371a407f
Boluwatife Arotiba <boluwatifearotiba09@gmail.com>
2022-09-22 13:03:23 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- *rot13 - encodes strings using rot13.
- *@s: pointer to string.
- *Return: pointer to encoded string.
+ *rot13 - encodes a string using rot13
+ *@s: input string.
+ *Return: the pointer to dest.
  */
 
 char *rot13(char *s)
 {
-	int stringCount, rotation;
-	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'			, 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't',			'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D'			, 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', '			O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 			'Y','Z'};
-	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'			, 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g'			, 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 				'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',			'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',			 'J', 'K', 'L', 'M'};
-	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
-	{
-		for (rotation = 0; rotation < 53; rotation++)
-		{
-			if (r1[rotation] == s[stringCount])
-			{
-				s[stringCount] = r2[rotation];
-				break;
-			}
-		}
-	}
-	return (s);
+int count = 0, i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+while (*(s + count) != '\0')
+{
+
+for (i = 0; i < 52; i++)
+{
+if (*(s + count) == alphabet[i])
+{
+*(s + count) = rot13[i];
+break;
+}
+}
+count++;
+}
+
+return (s);
 }
